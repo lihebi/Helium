86ef4fd32530e040b4306c4f260c49ff8761bc85
Hebi Li <lihebi.com@gmail.com>
2015-09-29 17:52:46 -0500

    use /tmp/helium_log.txt as Logger file

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -8,13 +8,14 @@ namespace fs = boost::filesystem;
 Logger* Logger::m_instance = 0;
 
 Logger::Logger() {
-  std::string folder = Config::Instance()->GetOutputFolder();
-  fs::path file_path(folder + "/default_log.txt");
-  fs::path dir = file_path.parent_path();
-  if (!fs::exists(dir)) {
-    fs::create_directories(dir);
-  }
-  m_logger.open(file_path.string());
+  // std::string folder = Config::Instance()->GetOutputFolder();
+  // fs::path file_path(folder + "/default_log.txt");
+  // fs::path dir = file_path.parent_path();
+  // if (!fs::exists(dir)) {
+  //   fs::create_directories(dir);
+  // }
+  // m_logger.open(file_path.string());
+  m_logger.open("/tmp/helium_log.txt");
   if (!m_logger.is_open()) {
     std::cerr << "Logger open failed." << std::endl;
     exit(1);
@@ -24,5 +25,8 @@ Logger::Logger() {
 
 void
 Logger::Log(const std::string& content) {
+  std::cout << "[Logger::Log]" << std::endl;
   m_logger << content << std::endl;
+  std::cout << content << std::endl;
+  // exit(1);
 }
