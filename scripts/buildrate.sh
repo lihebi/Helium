922073f0926b5efdc33c2becf5b96e8dbc779204
Hebi Li <lihebi.com@gmail.com>
2015-10-15 00:16:21 -0500

    buildrate script is ready

--- scripts/buildrate.sh
+++ scripts/buildrate.sh
@@ -3,8 +3,13 @@
 for fname in $1/*
 do
   echo $fname
-  # helium --pre $fname
-  # helium $fname
+  helium --pre $fname > /dev/null 2>&1
+  gtimeout 60 helium $fname > /dev/null 2>&1
+  if [ $? == 0 ]; then
+    echo "success";
+  else
+    echo "timeout";
+  fi
   success_count=`cat /tmp/helium_buildrate.txt | grep success | wc -l`
   all_count=`cat /tmp/helium_buildrate.txt | wc -l`
   buildrate=`echo "scale=2; $success_count/$all_count" | bc`
