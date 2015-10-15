af57baa6ca87f6e9602740b8c0b78e3f7b56c5b4
Hebi Li <lihebi.com@gmail.com>
2015-10-15 18:42:23 -0500

    csv format

--- scripts/buildrate.sh
+++ scripts/buildrate.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-
+field1="project name"
+field2="exit status"
+field3="compile success count"
+field4="compile error count"
+field5="build rate"
+field6="run success count"
+field7="run error count"
+field8="run rate"
+echo $field1,$field2,$field3,$field4,$field5
 for fname in $1/*
 do
-  echo $fname
-  # helium --pre $fname > /dev/null 2>&1
+  echo -n $fname,
   if [ `uname` == "Darwin" ]; then
     gtimeout 60 helium $fname > /dev/null 2>&1
   else
     timeout 60 helium $fname > /dev/null 2>&1
   fi
-  if [ $? == 0 ]; then
-    echo "success";
-  else
-    echo "timeout";
-  fi
-  success_count=`cat /tmp/helium/log/rate.txt | grep success | wc -l`
-  error_count=`cat /tmp/helium/log/rate.txt | grep error | wc -l`
+  success_count=`cat /tmp/helium/log/rate.txt | grep "compile success" | wc -l`
+  error_count=`cat /tmp/helium/log/rate.txt | grep "compile error" | wc -l`
   buildrate=`echo "scale=2; $success_count/($success_count+$error_count)" | bc`
-  echo SUCCESS: $success_count ERROR: $error_count RATE: $buildrate
+  echo $?, $success_count, $error_count, $buildrate
 done
