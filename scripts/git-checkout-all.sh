f6e5a3be60be6f35027b31d4fd31f30e1cd4e4c8
Hebi Li <lihebi.com@gmail.com>
2015-10-15 12:04:00 -0500

    fix cannot cd back bug

--- scripts/git-checkout-all.sh
+++ scripts/git-checkout-all.sh
@@ -3,11 +3,13 @@
 # checkout all prjects in folder
 
 cd $1
+AB_PATH=`pwd`
 if [ $? != 0 ]; then exit 1; fi
 for fname in *
 do
   echo checking out: $fname
-  cd $fname && git checkout -- . && cd ..
+  cd $fname && git checkout -- .
+  cd $AB_PATH
   if [ $? == 0 ]; then
     echo "OK"
   else
