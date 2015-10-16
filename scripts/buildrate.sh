a33606a639a4e9ec78985c6fbdd00c37ba574f0a
Hebi Li <lihebi.com@gmail.com>
2015-10-15 22:37:33 -0500

    move $? to the right position

--- scripts/buildrate.sh
+++ scripts/buildrate.sh
@@ -13,11 +13,13 @@ do
   echo -n $fname,
   if [ `uname` == "Darwin" ]; then
     gtimeout 60 helium $fname > /dev/null 2>&1
+    echo -n $?,
   else
     timeout 60 helium $fname > /dev/null 2>&1
+    echo -n $?,
   fi
   success_count=`cat /tmp/helium/log/rate.txt | grep "compile success" | wc -l`
   error_count=`cat /tmp/helium/log/rate.txt | grep "compile error" | wc -l`
   buildrate=`echo "scale=2; $success_count/($success_count+$error_count)" | bc`
-  echo $?, $success_count, $error_count, $buildrate
+  echo $success_count, $error_count, $buildrate
 done
