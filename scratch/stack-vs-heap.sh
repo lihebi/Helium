300d0b5ad89265d8e4a29506184c7ec4734d3a5f
Hebi Li <lihebi.com@gmail.com>
2016-01-26 13:20:10 -0600

    valgrind test

--- scratch/stack-vs-heap.sh
+++ scratch/stack-vs-heap.sh
@@ -19,20 +19,19 @@
 
 # ./a.out 1 $(( RANDOM % 10000 ))
 # $1 must be in {1,2,3,4}
-for i in {1..1000}
+for i in {1..100}
 do
-    echo $i
     myrand=$(( RANDOM % 10000 ))
     # echo -n $myrand " "
-    ./a.out 1 $myrand >/dev/null 2>&1
-    # valgrind --xml=yes --xml-file=out.xml ./a.out 2 $myrand >/dev/null 2>&1
-    # if [[ $? != 0 ]]; then
-    #     echo "fail: " $myrand
-    # else
-    #     echo "success: " $myrand
-    # fi
-    # ./valgrind-parser out.xml 85
-    # if [[ $? == 0 ]]; then
-    #     echo "valgrind correct failure: " $myrand
-    # fi
+    # ./a.out $1 $myrand >/dev/null 2>&1
+    valgrind --xml=yes --xml-file=out.xml ./a.out $1 $myrand >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo "fail: " $myrand
+    else
+        echo "success: " $myrand
+    fi
+    ./valgrind-parser out.xml 85
+    if [[ $? == 0 ]]; then
+        echo "valgrind correct failure: " $myrand
+    fi
 done
