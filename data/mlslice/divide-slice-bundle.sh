6ad5167b3b9916ca405698b07c5fdccb24005cd0
Hebi Li <lihebi.com@gmail.com>
2016-04-13 15:43:08 -0500

    add divide-slice bundle

---
+++ data/mlslice/divide-slice-bundle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# divide a bunch of files
+
+if [ $# == 0 ]; then
+    echo "Divide a bunch of slice files."
+    echo ""
+    echo "Usage: <this-script> /path/to/slice/file/folder"
+    echo ""
+    echo "Side Effect: ./tmp/ directory will be overwrote"
+    echo "Output: the folder inside slice-bundle/ with extension removed"
+    exit 1;
+fi
+
+mkdir slice-bundle
+
+for file in $1/*.txt; do
+    if [ -f $file ]; then
+        dir=${file##.*/} # remove traling directories
+        dir=${dir%\.*} # remove extension
+        dir=slice-bundle/$dir
+        echo $file , output to $dir
+        rm -rf $dir # I feel this is dangeous
+        ./divide-slice.sh $file
+        mv tmp $dir
+    fi
+done
