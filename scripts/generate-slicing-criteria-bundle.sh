1f7443f574bb798d463a3f470116be125617c2a6
Hebi Li <lihebi.com@gmail.com>
2016-04-13 11:43:13 -0500

    add slicing criteria bundle generation

---
+++ scripts/generate-slicing-criteria-bundle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# generate input.txt for code surfer slicing plugin
+
+# algorithm description
+# scan all files in the target benchmark directory
+# for each file, get its loc
+# randomly assign N lines to be in the slicing creteria, append them to "input.txt"
+
+if [ $# == 0 ]; then
+    echo "Generate for many benchmarks. Generate input.txt as slicing creteria."
+    echo ""
+    echo "Each line contains the absolute file name and a line number, as the slicing criteria."
+    echo "See each "
+    echo ""
+    echo "Usage: <this-script> /path/to/folder/containing/many/benchmarks"
+    echo "output: <individual-benchmark>/input.txt"
+    echo "Side effect: will overwrite input.txt for the current directory."
+    exit 1;
+fi
+
+for $folder in $1; do
+    generate-slicing-criteria.sh $folder $2
+    mv input.txt $folder
+done
