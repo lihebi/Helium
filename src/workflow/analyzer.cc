4b248f957546ce472112046a91d0e057452fc88d
Hebi Li <lihebi.com@gmail.com>
2016-10-26 11:24:59 -0500

    Add Z3 for post checking

--- src/workflow/analyzer.cc
+++ src/workflow/analyzer.cc
@@ -2,6 +2,7 @@
 #include "utils/utils.h"
 #include "helium_options.h"
 #include <iostream>
+#include <gtest/gtest.h>
 
 static const std::string TRANSFER_SCRIPT = "helium-analyze-transfer.R";
 static const std::string META_SCRIPT = "helium-analyze-meta.R";
@@ -62,6 +63,120 @@ bool entry_point(std::string s) {
   return true;
 }
 
+
+
+
+
+std::string get_declare_fun(std::string var) {
+  if (var.find("int") != std::string::npos) {
+    return "(declare-fun " + var + "() Int)";
+  } else if (var.find("bool") != std::string::npos) {
+    return "(declare-fun " + var + "() Bool)";
+  } else {
+    return "(declare-fun " + var + "() Real)";
+  }
+}
+
+std::string get_rhs(std::string s) {
+  if (s.find_first_of("+-*") != std::string::npos) {
+    std::string ret;
+    int idx = s.find_first_of("+-*");
+    std::string lhs = s.substr(0,idx);
+    std::string rhs = s.substr(idx+1);
+    utils::trim(lhs);
+    utils::trim(rhs);
+    char op = s[idx];
+    ret += "(" + std::string(1, op) + " " + lhs + " ";
+    ret += get_rhs(rhs) + ")";
+    return ret;
+  } else {
+    return s;
+  }
+}
+
+std::string get_assert(std::string constraint) {
+  std::string ret;
+  ret += "(assert ";
+  // only support binary operation
+  std::vector<std::string> hs = utils::split(constraint, "<=>");
+  if (hs.size() != 2) {
+    std::cerr << "not binary: " << constraint << "\n";
+    std::cout << "actuall size: " << hs.size() << "\n";
+    return "(assert (= 0 1))";
+  }
+  if (constraint.find('=') != std::string::npos) {
+    ret += "(= ";
+  } else if (constraint.find(">") != std::string::npos) {
+    ret += "(> ";
+  } else {
+    ret += "(< ";
+  }
+  std::string lhs = hs[0];
+  std::string rhs = hs[1];
+  utils::trim(lhs);
+  utils::trim(rhs);
+  ret += lhs + " ";
+  ret += get_rhs(rhs);
+  ret += ") )";
+  return ret;
+}
+
+bool check_sat(std::vector<std::string> vorig) {
+  // escape
+  std::vector<std::string> v;
+  for (std::string s : vorig) {
+    utils::replace(s, "[", ".");
+    utils::replace(s, "]", ".");
+    v.push_back(s);
+  }
+  // actual work
+  std::set<std::string> vars;
+  for (std::string s : v) {
+    std::vector<std::string> all = utils::split(s, "><=+- ");
+    for (std::string var : all) {
+      if (var.find("output") == 0 || var.find("input") == 0) {
+        vars.insert(var);
+      }
+    }
+  }
+  std::string smt;
+  for (std::string var : vars) {
+    smt += get_declare_fun(var) + "\n";
+  }
+  for (std::string s : v) {
+    smt += get_assert(s) + "\n";
+  }
+  smt += "(check-sat)";
+  // write to a smt file
+  std::string dir = utils::create_tmp_dir();
+  std::string smt_file = dir + "/helium.smt";
+  utils::write_file(smt_file, smt);
+  std::cout << "output smt to " << smt_file<< "\n";
+  // call z3 to execute
+  // FIXME check whether z3 is available
+  std::string cmd = "z3 -v:1 -smt2 " + smt_file;
+  std::string output = utils::new_exec(cmd.c_str());
+  utils::trim(output);
+  if (output == "sat") {
+    return true;
+  } else {
+    std::cout << "SAT output:" << "\n";
+    std::cout << output << "\n";
+    return false;
+  }
+}
+
+TEST(SATCase, SATTest) {
+  std::vector<std::string> v;
+  v.push_back("output_int_x > 8");
+  v.push_back("output_int_y < 7");
+  EXPECT_TRUE(check_sat(v));
+  v.push_back("output_int_y > output_int_x");
+  EXPECT_FALSE(check_sat(v));
+}
+
+
+
 /**
  * Analyze if the failure condition is going to be satisfied only depends on the program entry point.
  */
@@ -111,14 +226,21 @@ void Analyzer::ResolveQuery(std::string failure_condition) {
     // std::vector<std::vector<std::string> > combinations = get_combinations(vv);
 
     // TODO NOW putting into z3 to validate it
-    std::cout << utils::GREEN << "== Query Resolved!" << utils::RESET << "\n";
     std::cout << "Using:" << "\n";
+    std::vector<std::string> v;
+    v.push_back(failure_condition);
     for (auto m : mapping) {
       std::cout << "\t" << m.first << " = " << *m.second.begin() << "\n";
+      v.push_back(m.first + " = " + *m.second.begin());
     }
     std::cout << "On top of failure condition:" << "\n";
     std::cout << "\t" << failure_condition << "\n";
-    exit(0);
+    if (check_sat(v)) {
+      std::cout << utils::GREEN << "== Query Resolved!" << utils::RESET << "\n";
+      exit(0);
+    } else {
+      std::cout << utils::RED << "== SAT unsatisfiable." << utils::RESET << "\n";
+    }
   } else {
     std::cout << "== Resolve failed." << "\n";
     std::cout << "The output variables in FC cannot be all mapped." << "\n";
