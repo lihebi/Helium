18aee59b51de5b06cba3b0be4c47e2efb998df31
Hebi Li <lihebi.com@gmail.com>
2016-11-10 01:51:37 -0600

    Add buildrate script

---
+++ scripts/hexp-buildrate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# build rate test script
+
+# $1: /path/to/folder/containing/benchmarks
+# $2: /path/to/headered/pois
+# Output: helium-result.txt
+
+# Also, use tee to run the script!
+
+# 2>&1 | tee -ai log.txt
+
+
+
+for name in $1/*; do
+    echo "============ " ${name##*/} | tee -a helium-result.txt
+    timeout 30m helium --run-test=false --segment-per-poi-limit=100 --valid-poi-limit=100 $name --poi-file=$2/${name##*/}.csv 2>&1 | tee -a helium-result.txt
+    [ $? == 143 ] && echo "====== Benchmark $name Timeout" | tee -a helium-result.txt
+done
