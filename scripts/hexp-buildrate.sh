c71b2cc4e0290a8e30405995c68e2eb12d9a1539
Hebi Li <lihebi.com@gmail.com>
2016-12-12 17:12:26 -0600

    better hexp-buildrate

--- scripts/hexp-buildrate.sh
+++ scripts/hexp-buildrate.sh
@@ -14,7 +14,19 @@
 
 for name in $1/*; do
     echo "============ " ${name##*/} | tee -a helium-result.txt
-    timeout 30m helium --run-test=false --instrument-io=false --segment-per-poi-limit=100 --valid-poi-limit=100 $name --poi-file=$2/${name##*/}.csv 2>&1 | tee -a helium-result.txt
-    [ $? == 143 ] && echo "====== Benchmark $name Timeout" | tee -a helium-result.txt
+    timeout 30m helium\
+            --run-test=false --instrument-io=false\
+            --segment-per-poi-limit=100 --valid-poi-limit=100\
+            $name --poi-file=$2/${name##*/}.csv\
+            2>&1 | tee -a helium-result.txt
+    RETVAL=$?
     rm -rf /tmp/helium*
+    # return code for timeout
+    if [ $RETVAL == 124 ]; then
+        echo "====== Benchmark $name Timeout" | tee -a helium-result.txt
+    # return code for C-c
+    elif [ $RETVAL == 130 ]; then
+        echo "====== Interrupted with C-c. Stop"
+        break
+    fi
 done
