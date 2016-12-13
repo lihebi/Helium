b86b882f262704d190d1d320246dd9604768c410
Hebi Li <lihebi.com@gmail.com>
2016-12-13 11:32:46 -0600

    better hexp-buildrate

--- scripts/hexp-buildrate.sh
+++ scripts/hexp-buildrate.sh
@@ -16,17 +16,28 @@ for name in $1/*; do
     echo "============ " ${name##*/} | tee -a helium-result.txt
     timeout 30m helium\
             --run-test=false --instrument-io=false\
+            --print-compile-error=true\
             --segment-per-poi-limit=100 --valid-poi-limit=100\
             $name --poi-file=$2/${name##*/}.csv\
             2>&1 | tee -a helium-result.txt
     RETVAL=$?
+
+    # remove the temp folder to avoid run out of disk space
+    # comment this out if you want to debug the generated code
     rm -rf /tmp/helium*
+
+    
     # return code for timeout
     if [ $RETVAL == 124 ]; then
         echo "====== Benchmark $name Timeout" | tee -a helium-result.txt
     # return code for C-c
     elif [ $RETVAL == 130 ]; then
-        echo "====== Interrupted with C-c. Stop"
-        break
+        echo "====== Interrupted with C-c. Enter to continue next benchmark, C-c to stop."
+        read text
+        continue
+        # break
     fi
+
+    ## use one C-c to back to terminal, ask for input
+    ## another C-c will end the program
 done
