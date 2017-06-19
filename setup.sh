853106b33ed4267d48eb4576920ecc3014f4e6b4
Hebi Li <lihebi.com@gmail.com>
2017-06-19 12:56:28 -0500

    fix hard coded config path

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mkdir -p $HOME/.helium.d
-mkdir -p $HOME/.helium.d/etc
+# mkdir -p $HOME/.helium.d/etc
 ln -sf `pwd`/helium.conf $HOME/.heliumrc
-ln -sf `pwd`/etc/headers.conf.d/system.conf $HOME/.helium.d/etc/
-ln -sf `pwd`/etc/headers.conf.d/third-party.conf $HOME/.helium.d/etc/
-ln -sf `pwd`/etc/blacklist.conf $HOME/.helium.d/etc
-ln -sf `pwd`/etc/system.json $HOME/.helium.d/etc/
-ln -sf `pwd`/etc/third-party.json $HOME/.helium.d/etc/
+# ln -sf `pwd`/etc/headers.conf.d/system.conf $HOME/.helium.d/etc/
+# ln -sf `pwd`/etc/headers.conf.d/third-party.conf $HOME/.helium.d/etc/
+# ln -sf `pwd`/etc/blacklist.conf $HOME/.helium.d/etc
+# ln -sf `pwd`/etc/system.json $HOME/.helium.d/etc/
+# ln -sf `pwd`/etc/third-party.json $HOME/.helium.d/etc/
+ln -sf `pwd`/etc $HOME/.helium.d/
