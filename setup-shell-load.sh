766e450d75d652f61989eae595644904adbdee79
Hebi Li <lihebi.com@gmail.com>
2017-06-20 14:55:37 -0500

    setup shell scripts

--- setup-shell-load.sh
+++ setup-shell-load.sh
@@ -4,7 +4,7 @@
 # instead, load it in ~/.bashrc the following:
 # source /path/to/helium_install/setup-shell.sh
 
-export HELIUM_HOME=$HOME/github/helium
+# export HELIUM_HOME=$HOME/github/helium
 export PATH=$HELIUM_HOME/bin:$HELIUM_HOME/scripts:$PATH
 export PATH=$HELIUM_HOME/scripts/analyze:$HELIUM_HOME/scripts/hexp:$PATH
 export PATH=$HELIUM_HOME/build/bin:$PATH
--- setup-shell.sh
+++ setup-shell.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # add load to ~/.bashrc
+echo "export HELIUM_HOME=$(pwd)" >> ~/.bashrc
 echo "source $(pwd)/setup-shell-load.sh" >> ~/.bashrc
