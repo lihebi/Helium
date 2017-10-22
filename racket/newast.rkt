0b02543c3f4bf00b06dd99c010bf1a32dc0a00ed
Hebi Li <lihebi.com@gmail.com>
2017-10-22 17:44:50 -0500

    in newast

---
+++ racket/newast.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+(require c)
+
+(define myast (car (parse-program "int a;")))
+
+(type:primitive-name (decl:vars-type myast))
+
+(id:label-name (type:struct-tag (decl:vars-type (car (parse-program "struct  A *a;")))))
+
+;; manage the threads
+;; (custodian-shutdown-all (current-custodian))
+
+(define-syntax-rule (debug sexp)
+  ;; (printf "[debug] ~v\n" sexp)
+  null
+  )
+
+(define (gen node)
+  (cond
+    [(not node) ""]
+    ;; ID
+    [(id:label? node) (debug "id:label")
+                      (symbol->string (id:label-name node))]
+    [(id:var? node) (debug "id:var")
+                    (symbol->string (id:var-name node))]
+    ;; TYPE
+    [(type:pointer? node) (debug "type:pointer")
+                          "*"]
+    [(type:struct? node) (debug "type:struct")
+                         (string-append
+                          "struct "
+                          (gen (type:struct-tag node)))]
+    [(type:primitive? node) "TODO"]
+    ;; DECL
+    [(decl:typedef? node) (debug "decl:typedef")
+                          (string-append
+                           "typedef "
+                           (gen (decl:typedef-type node))
+                           " "
+                           (string-join
+                            (map gen (decl:typedef-declarators node))
+                            ",")
+                           ";")]
+    [(decl:declarator? node) (debug "decl:declarator")
+                             (string-append
+                              (gen (decl:declarator-type node))
+                              " "
+                              (gen (decl:declarator-id node))
+                              ;; TODO init
+                              )]
+    [(decl:function? node) "TODO"]
+    [else (debug node) (print node) (error "debug here")]))
+
+(gen (car (parse-program "typedef struct XXX *aaa, DDD;")))
+
+(gen (car (parse-program
+           (string->path
+            "/home/hebi/github/benchmark/craft/grammar/a.c"))))
