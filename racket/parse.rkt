9a739062d89647e0f26a5b2f5aef829b7f24f2c0
Hebi Li <lihebi.com@gmail.com>
2017-10-22 22:14:10 -0500

    finish typedefs from system folder

--- racket/parse.rkt
+++ racket/parse.rkt
@@ -13,14 +13,58 @@
   (let ([cmd (string-append
               "ctags -f - " path
               " | "
-              ""
               "awk 'BEGIN {FS=\"\\t\"} {if ($4 == \"t\") print $1}'")])
     ;; (displayln cmd)
     (string-split (with-output-to-string (lambda () (system cmd))))))
 
-(get-typedefs "/usr/include/dirent.h")
+(define (get-dir-typedefs dir #:exclude [ex ""])
+  (let ([cmd (string-append
+              "ctags -f - -R --exclude="
+              ex
+              " "
+              dir
+              " | "
+              "awk 'BEGIN {FS=\"\\t\"} {if ($4 == \"t\") print $1}'")])
+    (string-split (with-output-to-string (lambda () (system cmd))))))
+
+;; (get-dir-typedefs "/usr/include" "*/boost/*")
+(define (load-or-create-typedefs)
+  "in the current directory, locate the file typedef.txt"
+  (let ([f "typedef.txt"])
+    (if (file-exists? f)
+        (map string->symbol (string-split (file->string f)))
+        (let ([typedefs (get-dir-typedefs
+                    "/usr/include" #:exclude "*/boost/*")])
+          (with-output-to-file f
+            (lambda ()
+              (for ([item typedefs])
+                (displayln item))))
+          typedefs))))
+
+(length (load-or-create-typedefs))
+
+;; (get-typedefs "/usr/include/dirent.h")
 
 
-;; (parse-program
-;;  (string->path
-;;   "/home/hebi/github/benchmark/buffer-overflow/prep-polymorph/polymorph.c"))
+(define predefined-typedefs '(DIR))
+
+(parse-program
+ (string->path
+  "/home/hebi/github/benchmark/buffer-overflow/prep-polymorph/polymorph.c")
+ #:typedef predefined-typedefs)
+
+
+;; TODO parse local typedefs
+
+(define (parse-dir dir)
+  (for ([f (in-directory dir)])
+    (when (file-exists? f)
+      (let ([ext (bytes->string/locale (path-get-extension f))])
+        (when (or (string=? ext ".c")
+                  (string=? ext ".h"))
+          (displayln (format "Parsing ~a" f))
+          (parse-program f #:typedef (load-or-create-typedefs)))))))
+(parse-dir "/home/hebi/github/benchmark/buffer-overflow/prep-polymorph/")
+
+(require racket/path)
+(bytes->string/locale (path-get-extension "~/tmp/include.json"))
