5eebe10aa90f5317dd5b3d59225323677294d99a
Hebi Li <lihebi.com@gmail.com>
2018-05-06 11:23:44 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ echo "" >> $HOME/.bashrc
 echo "## ========== Helium Configuration" >> $HOME/.bashrc
 echo "" >> $HOME/.bahsrc
 echo "export HELIUM_HOME=$DIR" >> $HOME/.bashrc
-echo "export PATH=$HELIUM_HOME/bin:$HELIUM_HOME/scripts:$PATH" >> $HOME/.bashrc
+echo 'export PATH=$HELIUM_HOME/bin:$HELIUM_HOME/scripts:$PATH' >> $HOME/.bashrc
 echo "export ASAN_OPTIONS=detect_leaks=0:detect_stack_use_after_scope=0" >> $HOME/.bashrc
 
 echo "## ========== End of Helium Configuration" >> $HOME/.bashrc
